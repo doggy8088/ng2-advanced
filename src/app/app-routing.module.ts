292190a6eaedec7f6f432ef0f7dda19c94d786b4
Will保哥 <doggy.huang@gmail.com>
2017-03-07 11:19:12 +0800

    04 練習預設路由、轉向路由、萬用路由、獨立的 Route 物件、拆分獨立的 TS 模組

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
-import { Page2Component } from './page2/page2.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Route } from '@angular/router';
 
 import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+
+import { fallbackRoute } from './fallback-route';
 
 const routes: Routes = [
-  { path: '', children: [] },
+  { path: '', redirectTo: '/page1', pathMatch: 'full' },
   { path: 'page1', component: Page1Component },
-  { path: 'page2', component: Page2Component }
+  { path: 'page2', component: Page2Component },
+  fallbackRoute
 ];
 
 @NgModule({
