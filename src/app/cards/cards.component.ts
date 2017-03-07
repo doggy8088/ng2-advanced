26d32a0ecfeae585fccba4f17f14de14532dd14b
Will保哥 <doggy.huang@gmail.com>
2017-03-07 15:01:11 +0800

    08 練習在 /cards 頁面透過注入的 Router 物件透過 Click 事件導覽到另一個頁面

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-cards',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) {
+
+  }
+
+  GoHome() {
+    this.router.navigateByUrl('/');
+  }
+
+  GoChartsFlot() {
+    this.router.navigate(['/charts', 'flot']);
+  }
 
   ngOnInit() {
   }
