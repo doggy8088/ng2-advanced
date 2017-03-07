e271d16cb10d392006b199866e86b8eb70f2ac76
Will保哥 <doggy.huang@gmail.com>
2017-03-07 13:17:54 +0800

    05 練習套用 WB0901P31-HTML5 版面，並建立 /dashboard 與 /cards 頁面 (含路由)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,15 @@ import { Routes, RouterModule, Route } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CardsComponent } from './cards/cards.component';
+
 import { fallbackRoute } from './fallback-route';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/page1', pathMatch: 'full' },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'cards', component: CardsComponent },
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
   fallbackRoute
