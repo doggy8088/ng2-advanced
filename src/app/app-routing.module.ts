063fa02c88e0227beb568e443e8942d7036db827
Will保哥 <doggy.huang@gmail.com>
2017-03-07 10:18:07 +0800

    01 練習設定 AppRoutingModule 並加入 /page1 與 /page2 頁面

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
+import { Page2Component } from './page2/page2.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { Page1Component } from './page1/page1.component';
+
 const routes: Routes = [
   {
-    path: '',
-    children: []
+    path: 'page1',
+    component: Page1Component
+  },
+  {
+    path: 'page2',
+    component: Page2Component
   }
 ];
 
