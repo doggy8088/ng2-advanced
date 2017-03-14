b5b65b1a8b59bb1b20ba8d105b8a31bd1beba350
Will保哥 <doggy.huang@gmail.com>
2017-03-14 10:10:00 +0800

    13 練習將 ChartsModule 改用延遲載入的方式定義 (包含路由設定)

--- src/app/charts/charts-routing.module.ts
+++ src/app/charts/charts-routing.module.ts
@@ -5,16 +5,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { FlotComponent } from './flot/flot.component';
 
 const routes: Routes = [
-  { path: '', component: LayoutComponent,
-    children: [
-      { path: 'charts',
-        children: [
-          { path: '', redirectTo: 'flot', pathMatch: 'full' },
-          { path: 'flot', component: FlotComponent }
-        ]
-      }
-    ]
-  }
+  { path: '', redirectTo: 'flot', pathMatch: 'full' },
+  { path: 'flot', component: FlotComponent }
 ];
 
 @NgModule({
