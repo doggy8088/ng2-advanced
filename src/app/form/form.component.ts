75b82bd24bca38a67d6f7fcbfd4496a8c12b448d
Will保哥 <doggy.huang@gmail.com>
2017-03-14 13:45:45 +0800

    16 練習範本表單的 NgForm 用法與注意事項

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-form',
@@ -14,4 +15,9 @@ export class FormComponent implements OnInit {
   ngOnInit() {
   }
 
+  doSubmit(f: NgForm) {
+    if(f.valid) {
+      console.log(f.value);
+    }
+  }
 }
